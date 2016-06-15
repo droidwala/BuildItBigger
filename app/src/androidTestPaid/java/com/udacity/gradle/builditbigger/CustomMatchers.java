8fd4c8f540cec810b05e186f135a13e304e9acc9
messiahan <punitdama@gmail.com>
2016-06-15 19:40:43 +0530

    Add AsyncTask Espresso Test for prod build

---
+++ app/src/androidTestPaid/java/com/udacity/gradle/builditbigger/CustomMatchers.java
@@ -0,0 +1,31 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.view.View;
+import android.widget.TextView;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+public class CustomMatchers {
+
+    public static Matcher<View> isTextViewEmpty(){
+        return new BoundedMatcher<View,TextView>(TextView.class) {
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Checking if Textview is empty or not");
+            }
+
+            @Override
+            protected boolean matchesSafely(TextView item) {
+                if(item.getText().toString().isEmpty()) {
+                    return false;
+                }
+                else{
+                    return true;
+                }
+            }
+        };
+    }
+}
--- app/src/androidTestPaid/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTestPaid/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -11,30 +11,23 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
-
 
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void checkTextView(){
-        //Check all widgets are displayed correctly
+    public void checkAsyncTaskResultForNull(){
         onView(withId(R.id.textview_main_askquestion)).check(matches(isDisplayed()));
         onView(withId(R.id.button_main_yes)).check(matches(isDisplayed()));
         onView(withId(R.id.button_main_no)).check(matches(isDisplayed()));
 
         onView(withId(R.id.button_main_yes)).perform(click());
 
-        onView(withContentDescription("Interstitial close button")).perform(click());
-
-        onView(withId(R.id.textview_disp_joke)).check(matches(notNullValue()));
+        onView(withId(R.id.textview_disp_joke)).check(matches(CustomMatchers.isTextViewEmpty()));
 
     }
 }
